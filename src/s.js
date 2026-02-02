80e17ae7e7e478a9d1ae6b889a43c96f922b2982
Fabian <fabian@energyaccessexplorer.org>
2026-02-02 18:54:00 +0100

    handle `mutant:host` from presets.csv

--- src/s.js
+++ src/s.js
@@ -167,10 +167,16 @@ async function presets_init() {
 				if (!preset) return;
 				if (!preset.datasets) preset.datasets = [];
 
+				let name = r['ds_name'];
+				const m = name.match(/(.*):(.*)/);
+
+				if (m) name = m[1];
+
 				const ds = {
-					"name":   r['ds_name'],
-					"weight": intornot(r['weight']),
-					"domain": {
+					"name":      name,
+					"weight":    intornot(r['weight']),
+					"selection": maybe(m, 2) ? [maybe(m, 2)] : undefined,
+					"domain":    {
 						"min": intornot(r['min']),
 						"max": intornot(r['max']),
 					},
